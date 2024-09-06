94d77116d706ce2078a5891ed7201310a0660a4d
Harsh kaushik <kaushikharsh9897@gmail.com>
2024-09-07 00:17:34 +0530

    added admin, company, geo modal

--- WasmDashboard/Models/Admin.cs
+++ WasmDashboard/Models/Admin.cs
@@ -17,26 +17,4 @@ namespace WasmDashboard.Models
         public Company Company { get; set; }
     }
 
-    public class Address
-    {
-        public string Street { get; set; }
-        public string Suite { get; set; }
-        public string City { get; set; }
-        public string Zipcode { get; set; }
-        public Geo Geo { get; set; }
-    }
-
-    public class Geo
-    {
-        public string Lat { get; set; }
-        public string Lng { get; set; }
-    }
-
-    public class Company
-    {
-        public string Name { get; set; }
-        public string CatchPhrase { get; set; }
-        public string Bs { get; set; }
-    }
-
 }
